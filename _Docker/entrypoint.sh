308dc35c8d8a45e8ce77bb22306be3ba561215e3
Felipe Barreiros <felipewww@outlook.com.br>
2020-02-01 19:47:02 -0300

    run migrate script on container startup

--- _Docker/entrypoint.sh
+++ _Docker/entrypoint.sh
@@ -6,5 +6,4 @@ NC='\033[0m' # No Color
 
 echo -e  "${GREEN} Composing API container... ${NC} ${YELLOW} ${NC}"
 
-npm install && \
-    tsc && npm run watch
+npm install && tsc && knex migrate:latest && npm run watch
